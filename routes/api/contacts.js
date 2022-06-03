e6dd1eb4aeab5a5b4cd20ca3a69e9c5eae0c36ff
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-06-03 15:49:35 +0300

    get

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,35 @@
 const express = require("express");
 
+const Joi = require("joi");
+
+const {
+  listContacts,
+  getContactById,
+  updateContact,
+  addContact,
+  removeContact,
+} = require("../../models/contacts");
+
+const createError = require("../../helpers");
+
 const router = express.Router();
 
+const validationShema = Joi.object({
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
+  number: Joi.number().required(),
+});
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message12" });
+  try {
+    const contacts = await listContacts();
+    res.json(contacts);
+    if (!contacts) {
+      return null;
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
